de77d4652b7b1e4494cae73e31fe0afdc00d28ab
Patrick P Francisco unknown student <ppf3@afsconnect1.njit.edu>
2015-01-30 12:56:47 -0500

    fixed a minor linking error in CTS project

--- src/projects/cts/Header.php
+++ src/projects/cts/Header.php
@@ -1,5 +1,5 @@
 <ul id="menu">
-    <li><a href="Index.php">Home</a></li>
+    <li><a href="index.php">Home</a></li>
     <li><a href="Login_Page.php">Login</a></li> 
     <li><a href="Normal_track_search.php">Track</a></li> 
     <li><a href="About.php">About Us</a></li>
